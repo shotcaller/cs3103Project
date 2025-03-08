f6cf04d684736eb6907215246e5d5be0c5a5a754
swanlake-birdy <155507982+swanlake-birdy@users.noreply.github.com>
2025-03-08 10:32:25 -0400

    tested stored procedures of Sierra and changes needed

--- db/unlikeBlog.sql
+++ db/unlikeBlog.sql
@@ -1,16 +1,16 @@
 DELIMITER //
 DROP PROCEDURE IF EXISTS unlikeBlog //
-CREATE PROCEDURE unlikeBlog(IN blogId INT)
+CREATE PROCEDURE unlikeBlog(IN blogId INT, IN userId INT)
 BEGIN
    IF NOT EXISTS (SELECT 1 FROM Blogs WHERE Blogs.blogId = blogId) THEN
       SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'This Blog does not exist';
    END IF;
 
-   IF NOT EXISTS (SELECT 1 FROM Users WHERE Users.userId = blogId) THEN
+   IF NOT EXISTS (SELECT 1 FROM Users WHERE Users.userId = userId) THEN
       SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'This User does not exist';
    END IF;
    
-   DELETE from LIKES where Likes.userId=userId AND Likes.blogId=blogId;
+   DELETE from Likes where Likes.userId=userId AND Likes.blogId=blogId;
 
    -- UPDATE Blogs
    -- SET likeCount = GREATEST(likeCount - 1, 0)
