4cf8659bebd915e7c3056f81c14591aca2c20ec4
shotcaller <ruturaj.ghodke77@gmail.com>
2025-03-19 01:53:19 -0300

    like sql

--- db/blogs/likeBlog.sql
+++ db/blogs/likeBlog.sql
@@ -1,22 +1,20 @@
 DELIMITER //
 DROP PROCEDURE IF EXISTS likeBlog //
-CREATE PROCEDURE likeBlog(IN blogId INT, IN userId INT)
+CREATE PROCEDURE likeBlog(IN blogIdIn INT, IN userIdIn INT)
 BEGIN
-   IF NOT EXISTS (SELECT 1 FROM Blogs WHERE Blogs.blogId = blogId) THEN
+   IF NOT EXISTS (SELECT 1 FROM Blogs WHERE Blogs.blogId = blogIdIn) THEN
       SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'This Blog does not exist';
    END IF;
 
-   IF NOT EXISTS (SELECT 1 FROM Users WHERE Users.userId = blogId) THEN
+   IF NOT EXISTS (SELECT 1 FROM Users WHERE Users.userId = blogIdIn) THEN
       SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'This User does not exist';
    END IF;
-   
-   INSERT INTO Likes(blogId, userId) VALUES (blogId, userId);
 
-   -- UPDATE Blogs
-   -- SET likeCount = likeCount + 1
-   -- WHERE Blogs.blogId = blogId;
-
+   IF EXISTS (SELECT 1 FROM Likes WHERE (Likes.userId = userIdIn AND Likes.blogId = blogIdIn)) THEN
+      SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Blog is already liked by the user';
+   END IF;
    
+   INSERT INTO Likes(blogId, userId) VALUES (blogIdIn, userIdIn);
 
 END //
 
