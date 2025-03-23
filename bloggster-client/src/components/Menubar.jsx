0dd969c798f576f18b8de65d7331f0907086d3a4
shotcaller <ruturaj.ghodke77@gmail.com>
2025-03-23 09:28:25 -0300

    Menu bar component

---
+++ bloggster-client/src/components/Menubar.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import AppBar from '@mui/material/AppBar'
+import { Box, Button, IconButton, Menu, MenuItem, Toolbar, Typography } from '@mui/material'
+import AccountCircle from '@mui/icons-material/AccountCircle';
+
+
+const Menubar = (props) => {
+
+  //Auth variable
+  let auth = props.auth || true;
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+        <AppBar>
+            <Toolbar>
+                <Typography variant='h6' component='div' sx={{ flexGrow: 1}}>
+                    Bloggster
+                </Typography>
+                {
+                  auth ? (
+                    <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+
+                  ) : <Button variant='outlined' color='inherit'>Login / Register</Button>
+                }
+            </Toolbar>
+        </AppBar>
+    </Box>
+    
+  )
+}
+
+export default Menubar
